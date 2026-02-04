81142aeee9c529130f340a501ddadbaaeacafdd2
nitishvision <nitishpoonia@visionvivante.com>
2026-02-04 11:55:54 +0530

    Fully Working

--- src/NavigationStack.js
+++ src/NavigationStack.js
@@ -68,13 +68,6 @@ const MainRoutes = {
   Profile: ProfileStack,
 };
 
-const tabLabels = {
-  Home: i18n.t('Tabs.Home'),
-  Cashback: i18n.t('Tabs.Cashback'),
-  Profile: i18n.t('Tabs.Profile'),
-  Booking: i18n.t('Tabs.Booking'),
-};
-
 // Auth Stack (Login, Register, etc.)
 const AuthStack = () => (
   <Stack.Navigator
@@ -87,118 +80,127 @@ const AuthStack = () => (
 );
 
 // Protected Tab Navigator
-const MainTabs = () => (
-  <Tab.Navigator
-    screenOptions={({route}) => ({
-      tabBarPressColor: 'transparent',
-      headerShown: false,
-      tabBarLabel: '',
-      tabBarStyle: {
-        height: Matrics.vs(70),
-        paddingBottom: Matrics.vs(5),
-        paddingTop: Matrics.vs(15),
-        backgroundColor: '#fff',
-        elevation: 8,
-        shadowOpacity: 0.1,
-        shadowRadius: 4,
-        shadowColor: '#000',
-        shadowOffset: {width: 0, height: -1},
-      },
-      tabBarItemStyle: {
-        width: Matrics.screenWidth / 4,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingHorizontal: Matrics.s(5),
-      },
-      tabBarIconStyle: {
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      tabBarButton: props => (
-        <Pressable
-          {...props}
-          android_ripple={{color: 'transparent'}}
-          android_disableSound={true}
-          hitSlop={0}
-        />
-      ),
-      // eslint-disable-next-line react/no-unstable-nested-components
-      tabBarIcon: ({focused}) => {
-        let iconSource;
-        switch (route.name) {
-          case 'Home':
-            iconSource = focused ? Images.HOUSE : Images.HOUSE_INACTIVE;
-            break;
-          case 'Profile':
-            iconSource = focused
-              ? Images.USER_PLACEHOLDER
-              : Images.USER_PLACEHOLDER;
-            break;
-          case 'Booking':
-            iconSource = focused
-              ? Images.BOOKING_ACTIVE
-              : Images.BOOKING_INACTIVE;
-            break;
-          case 'Cashback':
-            iconSource = focused
-              ? Images.REFERRAL_ACTIVE
-              : Images.REFERRAL_INACTIVE;
-            break;
-          default:
-            iconSource = Images.HOUSE_INACTIVE;
-        }
+const MainTabs = () => {
+  const tabLabels = {
+    Home: i18n.t('Tabs.Home'),
+    Cashback: i18n.t('Tabs.Cashback'),
+    Profile: i18n.t('Tabs.Profile'),
+    Booking: i18n.t('Tabs.Booking'),
+  };
 
-        // Return the image component with custom label
-        return (
-          <View style={{alignItems: 'center', justifyContent: 'center'}}>
-            <View
-              style={{
-                borderTopWidth: focused ? 2 : 0, // Width 2 when focused
-                borderTopColor: COLOR.PRIMARY, // Primary color
-                width: Matrics.s(60), // Full tab width
-                height: focused ? 2 : 0, // Ensure the View has height when focused
-                position: 'absolute', // Position at the top
-                top: -16, // Align with the top of the tab item
-                zIndex: 1, // Ensure it's above other content
-              }}
-            />
-            <Image
-              source={iconSource}
-              style={{
-                height: focused ? 25 : 23,
-                width: Matrics.s(25),
-                opacity: focused ? 1 : 0.8,
-                resizeMode: 'contain',
-              }}
-            />
-            <Text
-              style={{
-                fontSize: focused
-                  ? typography.fontSizes.fs14
-                  : typography.fontSizes.fs12,
-                fontFamily: focused
-                  ? typography.fontFamily.Montserrat.SemiBold
-                  : typography.fontFamily.Montserrat.Medium,
-                color: focused ? COLOR.PRIMARY : COLOR.DIM_TEXT_COLOR,
-                marginTop: Matrics.vs(2),
-                opacity: focused ? 1 : 0.8,
-                textAlign: 'center',
-                width: '100%',
-                paddingHorizontal: Matrics.s(2),
-              }}
-              numberOfLines={1}
-              ellipsizeMode="tail">
-              {tabLabels[route.name]}
-            </Text>
-          </View>
-        );
-      },
-    })}>
-    {Object.keys(MainRoutes).map(route => (
-      <Tab.Screen key={route} name={route} component={MainRoutes[route]} />
-    ))}
-  </Tab.Navigator>
-);
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarPressColor: 'transparent',
+        headerShown: false,
+        tabBarLabel: '',
+        tabBarStyle: {
+          height: Matrics.vs(70),
+          paddingBottom: Matrics.vs(5),
+          paddingTop: Matrics.vs(15),
+          backgroundColor: '#fff',
+          elevation: 8,
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          shadowColor: '#000',
+          shadowOffset: {width: 0, height: -1},
+        },
+        tabBarItemStyle: {
+          width: Matrics.screenWidth / 4,
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingHorizontal: Matrics.s(5),
+        },
+        tabBarIconStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        tabBarButton: props => (
+          <Pressable
+            {...props}
+            android_ripple={{color: 'transparent'}}
+            android_disableSound={true}
+            hitSlop={0}
+          />
+        ),
+        // eslint-disable-next-line react/no-unstable-nested-components
+        tabBarIcon: ({focused}) => {
+          let iconSource;
+          switch (route.name) {
+            case 'Home':
+              iconSource = focused ? Images.HOUSE : Images.HOUSE_INACTIVE;
+              break;
+            case 'Profile':
+              iconSource = focused
+                ? Images.USER_PLACEHOLDER
+                : Images.USER_PLACEHOLDER;
+              break;
+            case 'Booking':
+              iconSource = focused
+                ? Images.BOOKING_ACTIVE
+                : Images.BOOKING_INACTIVE;
+              break;
+            case 'Cashback':
+              iconSource = focused
+                ? Images.REFERRAL_ACTIVE
+                : Images.REFERRAL_INACTIVE;
+              break;
+            default:
+              iconSource = Images.HOUSE_INACTIVE;
+          }
+
+          // Return the image component with custom label
+          return (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <View
+                style={{
+                  borderTopWidth: focused ? 2 : 0, // Width 2 when focused
+                  borderTopColor: COLOR.PRIMARY, // Primary color
+                  width: Matrics.s(60), // Full tab width
+                  height: focused ? 2 : 0, // Ensure the View has height when focused
+                  position: 'absolute', // Position at the top
+                  top: -16, // Align with the top of the tab item
+                  zIndex: 1, // Ensure it's above other content
+                }}
+              />
+              <Image
+                source={iconSource}
+                style={{
+                  height: focused ? 25 : 23,
+                  width: Matrics.s(25),
+                  opacity: focused ? 1 : 0.8,
+                  resizeMode: 'contain',
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: focused
+                    ? typography.fontSizes.fs14
+                    : typography.fontSizes.fs12,
+                  fontFamily: focused
+                    ? typography.fontFamily.Montserrat.SemiBold
+                    : typography.fontFamily.Montserrat.Medium,
+                  color: focused ? COLOR.PRIMARY : COLOR.DIM_TEXT_COLOR,
+                  marginTop: Matrics.vs(2),
+                  opacity: focused ? 1 : 0.8,
+                  textAlign: 'center',
+                  width: '100%',
+                  paddingHorizontal: Matrics.s(2),
+                }}
+                numberOfLines={1}
+                ellipsizeMode="tail">
+                {tabLabels[route.name]}
+              </Text>
+            </View>
+          );
+        },
+      })}>
+      {Object.keys(MainRoutes).map(route => (
+        <Tab.Screen key={route} name={route} component={MainRoutes[route]} />
+      ))}
+    </Tab.Navigator>
+  );
+};
 
 // Main Navigation Stack
 const NavigationStack = () => {
